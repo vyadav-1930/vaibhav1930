4d1fef70520458018082f36a96a0e0eb22e94ae4
vyadav-1930 <120701268+vyadav-1930@users.noreply.github.com>
2022-12-19 10:39:14 +0530

    Create input array.cpp

---
+++ input array.cpp
@@ -0,0 +1,20 @@
+	#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,sum;
+	int i;
+	cin>>n;
+	int arr[n];
+	for(i=0;i<n;i++)
+	{
+		cin>>arr[i];
+		
+	}
+		for(i=0;i<n;i++){
+			cout<<arr[i]<<endl;
+			sum=arr[i]+sum;
+		
+		}
+			cout<<sum;
+}
